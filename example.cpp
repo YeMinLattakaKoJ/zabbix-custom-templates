9e14fbdbe1452ddc16f611b39b22dacee8c4e390
Ye Min Latt <128687748+YeMinLattakaKoJ@users.noreply.github.com>
2025-09-18 09:50:08 +0630

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,13 @@
+zabbix-custom-templates/
+├── templates/
+│   ├── network-devices/
+│   │   ├── template-network-device-snmp.yaml
+│   │   ├── template-f5-bigip-pool-member-snmp.yaml
+│   │   └── template-efficient-ip-snmp.yaml
+│   ├── databases/
+│   │   └── template-patroni-db.yaml
+│   ├── applications/
+│   │   └── template-api-healthcheck-dynamic-token.yaml
+│   └── servers/
+│       └── template-linux-custom.yaml
+└── README.md
